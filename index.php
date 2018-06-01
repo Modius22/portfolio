2b92536712654c51034aebb49ca5058cd5e85f08
Julian Finkler <julian@mintware.de>
2018-06-01 23:00:14 +0200

    Silex removed and code migrated

--- index.php
+++ index.php
@@ -1,6 +1,9 @@
 <?php
 
 
+use Symfony\Component\Form\Forms;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Matcher\UrlMatcher;
 use Symfony\Component\Routing\RequestContext;
 use Symfony\Component\Routing\RouteCollection;
@@ -9,17 +12,17 @@ use Symfony\Component\Routing\Route;
 
 require_once __DIR__ . '/vendor/autoload.php';
 
-$app = new Silex\Application();
-$app['debug'] = true;
+define('DEBUG', true);
 
-$app->register(new Silex\Provider\TwigServiceProvider(), array(
-    'twig.path' => __DIR__ . '/resources',
-));
+$twigLoader = new Twig\Loader\FilesystemLoader(__DIR__ . '/resources');
+$twig = new Twig\Environment($twigLoader, [
+    'debug' => DEBUG
+]);
 
+$mailer = new Swift_Mailer(new Swift_Transport_NullTransport(new Swift_Events_SimpleEventDispatcher()));
 
-
-$app['twig']->addFunction(new Twig_Function('resource', function ($name, $embed = false, $base64 = true) use ($app) {
-    $globals = $app['twig']->getGlobals();
+$twig->addFunction(new Twig_Function('resource', function ($name, $embed = false, $base64 = true) use ($twig) {
+    $globals = $twig->getGlobals();
     $resources = $globals['resources'];
     $result = $resources[$name];
     if (substr($result, 0, 1) == '@') {
@@ -34,81 +37,82 @@ $app['twig']->addFunction(new Twig_Function('resource', function ($name, $embed 
     return $result;
 }));
 
-$app['twig']->addFilter(new Twig_Filter('values', function ($array) {
+$twig->addFilter(new Twig_Filter('values', function ($array) {
     return array_values($array);
 }));
 
 
-$app->get('/', function () use ($app) {
-    $jsonFile = __DIR__ . '/resources/meta.json';
-    $templateData = json_decode(file_get_contents($jsonFile), true);
-    $res = [];
-    if (isset($templateData['resources'])) {
-        $res = $templateData['resources'];
-    }
-    $app['twig']->addGlobal('resources', $res);
-
-    foreach ($templateData['sections'] as $sectionName => $section) {
-        if (isset($section['description']) && !isset($section['template'])) {
-            $templateData['sections'][$sectionName]['description'] = $app['twig']->createTemplate($section['description'])->render($templateData);
+$routes = new RouteCollection();
+$routes->add('index', new Route('/', [
+    '_controller' => function () use ($twig) {
+        $jsonFile = __DIR__ . '/resources/meta.json';
+        $templateData = json_decode(file_get_contents($jsonFile), true);
+        $res = [];
+        if (isset($templateData['resources'])) {
+            $res = $templateData['resources'];
         }
-    }
+        $twig->addGlobal('resources', $res);
 
-    $templateFile = 'template.html.twig';
-    if (is_file(__DIR__ . '/resources/template.custom.html.twig')) {
-        $templateFile = 'template.custom.html.twig';
-    }
-    return $app['twig']->render($templateFile, $templateData);
-});
-
-// Setup for email Custom_Background::attachment_fields_to_edit
-
-$app->register(new Silex\Provider\SwiftmailerServiceProvider());
-
-$app['swiftmailer.options'] = array(
-    'host' => 'PLACEHOLDER',
-    'port' => '25',
-    'username' => 'PLACEHOLDER',
-    'password' => 'PLACEHOLDER',
-    'encryption' => null,
-    'auth_mode' => null
-);
-
-$app->match('/contact', function(Request $request) use ($app) {
-    $form = $app['form.factory']->createBuilder('form')
-        ->add('name', 'text')
-        ->add('message', 'textarea')
-        ->getForm();
-    $request = $app['request'];
-    if ($request->isMethod('POST'))
-    {
-        $form->bind($request);
-        if ($form->isValid())
-        {
-            $data = $form->getData();
-            $messagebody = $data['message'];
-            $name        = $data['name'];
-            $subject = "Message from ".$name;
-            $app['mailer']->send(\Swift_Message::newInstance()
-                ->setSubject($subject)
-                ->setFrom(array('PLACEHOLDER')) // replace with your own
-                ->setTo(array('PLACEHOLDER'))   // replace with email recipient
-                ->setBody($app['twig']->render(__DIR__ . '/resources/sections/email.html.twig',   // email template
-                    array('name'      => $name,
-                          'message'   => $messagebody,
-                    )),'text/html'));
+        foreach ($templateData['sections'] as $sectionName => $section) {
+            if (isset($section['description']) && !isset($section['template'])) {
+                $templateData['sections'][$sectionName]['description'] = $twig->createTemplate($section['description'])->render($templateData);
+            }
         }
-        return $app['twig']->render(__DIR__ . '/resources/sections/email.html.twig', array(
-            'message' => 'Message Sent',
-            'form' => $form->createView()
+
+        $templateFile = 'template.html.twig';
+        if (is_file(__DIR__ . '/resources/template.custom.html.twig')) {
+            $templateFile = 'template.custom.html.twig';
+        }
+        return new Response($twig->render($templateFile, $templateData));
+    }
+]));
+
+/** @var Request $request */
+$request = Request::createFromGlobals();
+$routes->add('contact', new Route('/contact', [
+    '_controller' => function () use ($request, $twig, $mailer) {
+
+        $form = Forms::createFormFactory()->createBuilder()
+            ->add('name', \Symfony\Component\Form\Extension\Core\Type\TextType::class)
+            ->add('message', \Symfony\Component\Form\Extension\Core\Type\TextareaType::class)
+            ->getForm();
+
+        if ($request->isMethod('POST')) {
+            $form->handleRequest();
+
+            if ($form->isSubmitted() && $form->isValid()) {
+                $data = $form->getData();
+                $messagebody = $data['message'];
+                $name = $data['name'];
+                $subject = "Message from " . $name;
+                $message = new Swift_Message($subject, $twig->render('sections/email.html.twig',   // email template
+                    array('name' => $name,
+                        'message' => $messagebody,
+                    )), 'text/html');
+                $message
+                    ->setFrom(array('hans@someone.tld'))// replace with your own
+                    ->setTo(array('foo@bar.tld'));// replace with email recipient
+                var_dump($mailer->send($message));
+            }
+            return new Response($twig->render('sections/email.html.twig', array(
+                'message' => 'Message Sent',
+                'form' => $form->createView()
+            )));
+        }
+        return new Response($twig->render('sections/email.html.twig', array(
+                'message' => 'Send message to us',
+                'form' => $form->createView()
+            )
         ));
     }
-    return $app['twig']->render(__DIR__ . '/resources/sections/email.html.twig', array(
-            'message' => 'Send message to us',
-            'form' => $form->createView()
-        )
-    );
-}, "GET|POST");
+]));
 
+$context = new RequestContext();
+$context->fromRequest($request);
 
-$app->run();
+$matcher = new UrlMatcher($routes, $context);
+$parameters = $matcher->match($request->getPathInfo());
+
+/** @var \Symfony\Component\HttpFoundation\Response $resp */
+$resp = call_user_func($parameters['_controller']);
+$resp->send();
